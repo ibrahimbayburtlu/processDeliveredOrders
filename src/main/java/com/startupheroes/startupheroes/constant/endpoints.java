e12ca2fecc904969b87608c5eb4474fc9a6cee71
İbrahim Bayburtlu <ibrahim.bayburtlu@enuygun.com>
2025-05-10 22:21:13 +0300

    refactor: translate comments to English in application.properties

---
+++ src/main/java/com/startupheroes/startupheroes/constant/endpoints.java
@@ -0,0 +1,7 @@
+package com.startupheroes.startupheroes.constant;
+
+public class endpoints {
+    public static final String ORDERS = "/orders";
+    public static final String ORDERS_ID = "/{id}";
+    public static final String ORDERS_PROCESS = "/process/{date}";
+}
